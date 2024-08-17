a4cbeda5ef039d4a9611b02c8772714e493ca8a2
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2024-08-18 00:10:43 +0530

    Completed sign up screen except for checkbox

---
+++ lib/sign_up_screen.dart
@@ -0,0 +1,310 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 22,
+                ),
+                SizedBox(
+                  height: 40,
+                  width: 42,
+                  child: Image.asset(
+                    "assets/eduverse_logo.png",
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                const Text(
+                  "Create a\nnew account",
+                  style: TextStyle(
+                    color: Color(
+                      0xff1E1F20,
+                    ),
+                    fontSize: 30,
+                    height: 1.2,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Container(
+                  height: 3,
+                  width: 24,
+                  decoration: BoxDecoration(
+                    color: const Color(
+                      0xff4178F3,
+                    ),
+                    borderRadius: BorderRadius.circular(
+                      2,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 36,
+                ),
+                const Text(
+                  "Email or Phone number",
+                  style: TextStyle(
+                    color: Color(
+                      0xff8F92A1,
+                    ),
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                TextFormField(
+                  style: const TextStyle(color: Color(0xff030319)),
+                  cursorColor: const Color(0xff030319),
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.only(
+                      left: 24,
+                    ),
+                    fillColor: const Color(
+                      0xff8F92A1,
+                    ).withOpacity(.3),
+                    filled: true,
+                    hintText: "Enter Email or Phone number",
+                    hintStyle: TextStyle(
+                      color: const Color(0xff030319).withOpacity(
+                        0.4,
+                      ),
+                    ),
+                    suffixIcon: const Icon(
+                      Icons.check_rounded,
+                      color: Color(
+                        0xff38D79F,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                const Text(
+                  "Password",
+                  style: TextStyle(
+                    color: Color(
+                      0xff8F92A1,
+                    ),
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  style: const TextStyle(color: Color(0xff030319)),
+                  cursorColor: const Color(0xff030319),
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.only(
+                      left: 24,
+                    ),
+                    fillColor: const Color(
+                      0xff8F92A1,
+                    ).withOpacity(.3),
+                    filled: true,
+                    hintText: "Enter password",
+                    hintStyle: TextStyle(
+                      color: const Color(0xff030319).withOpacity(
+                        0.4,
+                      ),
+                    ),
+                    suffixIcon: const Icon(
+                      Icons.remove_red_eye_rounded,
+                      color: Color(
+                        0xff8F92A1,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                const Text(
+                  "Confirm password",
+                  style: TextStyle(
+                    color: Color(
+                      0xff8F92A1,
+                    ),
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  style: const TextStyle(color: Color(0xff030319)),
+                  cursorColor: const Color(0xff030319),
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.only(
+                      left: 24,
+                    ),
+                    fillColor: const Color(
+                      0xff8F92A1,
+                    ).withOpacity(.3),
+                    filled: true,
+                    hintText: "Enter confirm password",
+                    hintStyle: TextStyle(
+                      color: const Color(0xff030319).withOpacity(
+                        0.4,
+                      ),
+                    ),
+                    suffixIcon: const Icon(
+                      Icons.remove_red_eye_rounded,
+                      color: Color(
+                        0xff8F92A1,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                Row(
+                  children: [
+                    // TODO: Checkbox
+                    const SizedBox(
+                      width: 12,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "By creating an account, you agree to our ",
+                          style: TextStyle(
+                            color: Color(
+                              0xff8F92A1,
+                            ),
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            // TODO: Show snack bar that this will open a link
+                          },
+                          child: const Text(
+                            "Terms and Conditions",
+                            style: TextStyle(
+                              color: Color(
+                                0xff1B08DA,
+                              ),
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => const SignUpScreen(),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    width: MediaQuery.of(context).size.width - 48,
+                    height: 44,
+                    decoration: BoxDecoration(
+                      color: const Color(
+                        0xff1B08DA,
+                      ),
+                      borderRadius: BorderRadius.circular(
+                        8,
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Create Account",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 76,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Already have a account? ",
+                        style: TextStyle(
+                          color: Color(
+                            0xff8F92A1,
+                          ),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          // TODO: Show snack bar that this will open a link
+                        },
+                        child: const Text(
+                          "Log in",
+                          style: TextStyle(
+                            color: Color(
+                              0xff1B08DA,
+                            ),
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
